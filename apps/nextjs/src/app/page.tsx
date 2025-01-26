969ce876c9326e4029d2f0b0ce50d6ecc2200e8e
Cesar <ccesarslv@gmail.com>
2025-01-26 16:15:59 -0300

    feat: add login button in marketing page

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 import { HydrateClient } from "@/trpc/server";
 
 export const runtime = "edge";
@@ -5,7 +8,11 @@ export const runtime = "edge";
 export default function HomePage() {
   return (
     <HydrateClient>
-      <main className="container h-screen py-16"></main>
+      <main className="container h-screen py-16">
+        <Button asChild>
+          <Link href={"/login"}>Entrar</Link>
+        </Button>
+      </main>
     </HydrateClient>
   );
 }
